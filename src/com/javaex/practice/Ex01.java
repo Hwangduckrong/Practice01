45d45cb39f0c98153f33dffbe1e13e6ebf01cb50
hwangdeoglyong <hwangdeoglyong@218.38.137.28>
2024-06-21 11:02:12 +0900

    2024-06-21 Ex01완

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,21 @@
+package com.javaex.practice;
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		
+		
+		/*다음중 기본 자료형중 정수형자료형이 아닌 것은? 
+				(1) int
+				(2) float 
+				(3) byte 
+				(4) long
+				
+				정답:2번 float, 실수형에 해당하는 기본자료형이다, 
+				*/
+
+	}
+
+
+		
+}
