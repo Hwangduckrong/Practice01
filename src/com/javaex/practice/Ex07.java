ccc1f5e40550a11eb26dbf660501ed038c3bb2c4
hwangdeoglyong <hwangsee12@naver.com>
2024-06-21 14:31:32 +0900

    7번 풀이

--- src/com/javaex/practice/Ex07.java
+++ src/com/javaex/practice/Ex07.java
@@ -4,7 +4,10 @@ public class Ex07 {
 
 	public static void main(String[] args) {
 	 
-	boolean 
+	boolean ace = true; //true는 식별자, 예약어 사용할 수 없다. boolean에서 사용됨.
+	int ofworkers = 36;//#_of_workersd의 #,_은 변수의 이름으로 사용할 수 없다. int를 사용하였기 때문에 정수로 표기.
+	int firstLevel = 1; //1은 변수의 이름으로 사용할 수 없다.
+	
 
 	}
 
--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -3,8 +3,8 @@ package com.javaex.practice;
 public class Ex08 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	 boolean ace = true;
+	 String 
 	}
 
 }
